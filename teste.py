76ee1e933539f291f8769b565e84ef65c54f6407
TheMrDengoso <43834934+TheMrDengoso@users.noreply.github.com>
2019-04-25 22:44:27 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,21 +2,27 @@ from FileHelper import lerArquivo
 conteudo = lerArquivo()
 nomes = []
 numeros = []
+agendas = []
 for i,e in enumerate(conteudo):
     if e != 'agenda' and e != 'chamadas':
         d_ponto = str(conteudo[i]).split(":")
         p = str(d_ponto[0]).split('-')
+        agenda = str(d_ponto[1]).split(",")
+        print(agenda[1])
         nomes.append(p[0])
-        numeros.append(p[1])   
+        numeros.append(p[1])
+        agendas.append(d_ponto[1])
     if e == 'chamadas':
         break
-print(nomes)
-print(numeros)
+
 
 
 while True:
     print("Menu: \n1 - Ver agenda de um suspeito \n2 - Listar agendas apenas com suspeitos incluídos \n3 - Visualizar reciprocidades \n4 - Visualizar contatos com alto nível de suspeição \n5 - Sair")
     opcao = int(input("Digite a opção deseja: "))
+    if opcao > 5 or opcao <= 0:
+      print("Valor inválido, tente o que está no menu!")
+      continue
     if opcao == 5:
         break
 
